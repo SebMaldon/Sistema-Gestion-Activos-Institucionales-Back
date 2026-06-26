65ead718262903a76cfa25a14fa3e942b065f6e5
SebMaldon <smmsebastian@gmail.com>
2026-06-26 13:52:08 -0700

    Mejoras de búsqueda

--- src/graphql/resolvers/bienes.resolver.ts
+++ src/graphql/resolvers/bienes.resolver.ts
@@ -269,7 +269,7 @@ export function applyBienesFilters(qb: any, filter?: BienesFilter): { needsTI: b
     if (isIP) {
       qb.andWhere(
         '(LTRIM(RTRIM(b.num_serie)) LIKE :s OR b.num_inv LIKE :s OR b.clave_presupuestal LIKE :s OR TRY_CAST(b.id_bien AS NVARCHAR(36)) LIKE :s ' +
-        'OR mod_search.descrip_disp LIKE :s ' +
+        'OR b.clave_modelo LIKE :s OR mod_search.descrip_disp LIKE :s OR ti_search.nombre_host LIKE :s ' +
         'OR ti_search.dir_ip = :exact ' +
         'OR ti_search.dir_ip LIKE :start ' +
         'OR ti_search.dir_ip LIKE :end ' +
@@ -286,7 +286,7 @@ export function applyBienesFilters(qb: any, filter?: BienesFilter): { needsTI: b
     } else {
       qb.andWhere(
         '(LTRIM(RTRIM(b.num_serie)) LIKE :s OR b.num_inv LIKE :s OR b.clave_presupuestal LIKE :s OR TRY_CAST(b.id_bien AS NVARCHAR(36)) LIKE :s OR ti_search.dir_ip LIKE :s ' +
-        'OR mod_search.descrip_disp LIKE :s ' +
+        'OR b.clave_modelo LIKE :s OR mod_search.descrip_disp LIKE :s OR ti_search.nombre_host LIKE :s ' +
         'OR EXISTS (SELECT 1 FROM Cuentas_PC cpc WHERE cpc.id_bien = b.id_bien AND (cpc.cuenta_windows LIKE :s OR cpc.correo LIKE :s)))',
         { s: `%${term}%` }
       );
