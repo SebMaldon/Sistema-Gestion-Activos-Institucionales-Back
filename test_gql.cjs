1b6fb6d0d1a9dec67148bbac5390854cc35c2028
SebMaldon <smmsebastian@gmail.com>
2026-07-01 09:39:15 -0700

    Limpieza

--- test_gql.cjs
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const id = '71183E24-1110-4146-9D27-3A9FFBBAEE14';
-const body = JSON.stringify({ query: `{ programasPC(id_bien: "${id}") { programa version } }` });
-const req = http.request(
-  { hostname: 'localhost', port: 4000, path: '/graphql', method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(body) } },
-  (res) => { let d = ''; res.on('data', c => d += c); res.on('end', () => console.log(d)); }
-);
-req.write(body); req.end();
--- test_hp2.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from './src/config/database';
-
-AppDataSource.initialize().then(async () => {
-  const res = await AppDataSource.query("SELECT * FROM Bienes WHERE id_bien = (SELECT id_bien FROM Especificaciones_TI WHERE dir_ip = '11.31.19.156')");
-  console.log(res);
-  process.exit(0);
-});
