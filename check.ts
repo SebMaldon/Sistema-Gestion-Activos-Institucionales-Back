fbe7e8f6efb7f96a91a7ed4558ca8f0e10c8e2f1
SebMaldon <smmsebastian@gmail.com>
2026-07-01 10:07:53 -0700

    Liempieza extra

--- check.ts
+++
@@ -1,8 +0,0 @@
-import { AppDataSource } from './src/config/database';
-AppDataSource.initialize().then(async () => {
-  const res1 = await AppDataSource.query('EXEC sp_columns Unidad_A_Cargo');
-  const res2 = await AppDataSource.query('EXEC sp_columns Contactos');
-  console.log('Unidad_A_Cargo:', res1.map((c: any) => c.COLUMN_NAME + ' ' + c.TYPE_NAME));
-  console.log('Contactos:', res2.map((c: any) => c.COLUMN_NAME + ' ' + c.TYPE_NAME));
-  process.exit(0);
-}).catch(console.error);
--- check3.ts
+++
@@ -1,8 +0,0 @@
-import { AppDataSource } from './src/config/database';
-AppDataSource.initialize().then(async () => {
-  const data1 = await AppDataSource.query('SELECT TOP 5 * FROM Unidad_A_Cargo');
-  console.log('Unidad_A_Cargo Data:', data1);
-  const data2 = await AppDataSource.query('SELECT TOP 5 * FROM Contactos');
-  console.log('Contactos Data:', data2);
-  process.exit(0);
-}).catch(console.error);
